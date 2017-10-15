71496f862ec19d70be49d5bdee76d94d00de5927
Kaushik Srinivasan <kausrini@gmail.com>
2017-10-15 19:33:54 +0000

    Added IPV6 rules for firewall

--- pi_setup_files/iptables.sh
+++ pi_setup_files/iptables.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#IPV4 RULES
+
+# Flush existing Input and Output policy
+iptables -F INPUT
+iptables -F OUTPUT
+
 # Default policy for Input and Output
 iptables -P OUTPUT  ACCEPT
 iptables -P INPUT  DROP
@@ -11,11 +17,9 @@ iptables -A INPUT ! -i lo -d 127.0.0.0/8 -j REJECT
 # Accepts all established inbound connections
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-
-# Allows HTTP and HTTPS connections from anywhere (Will be blocking 8080 in future after setting up reverse proxy)
+# Allows HTTP and HTTPS connections from anywhere
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
 
 # Allow SSH connections 
 iptables -A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT
@@ -28,3 +32,53 @@ iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
 
 # log iptables denied calls (access via 'dmesg' command)
 iptables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
+
+
+#IPV6 RULES
+
+# Flush chains
+ip6tables -F INPUT
+ip6tables -F FORWARD
+ip6tables -F OUTPUT
+
+# Set up default policies
+ip6tables -P INPUT DROP
+ip6tables -P FORWARD DROP
+ip6tables -P OUTPUT ACCEPT
+
+# Allow localhost traffic.
+ip6tables -A INPUT -s ::1 -d ::1 -j ACCEPT
+
+# Allow some ICMPv6 types in the INPUT chain
+# Using ICMPv6 type names to be clear.
+ip6tables -A INPUT -p icmpv6 --icmpv6-type destination-unreachable -j ACCEPT
+ip6tables -A INPUT -p icmpv6 --icmpv6-type packet-too-big -j ACCEPT
+ip6tables -A INPUT -p icmpv6 --icmpv6-type time-exceeded -j ACCEPT
+ip6tables -A INPUT -p icmpv6 --icmpv6-type parameter-problem -j ACCEPT
+
+# Allow some other types in the INPUT chain, but rate limit.
+ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-request -m limit --limit 900/min -j ACCEPT
+ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-reply -m limit --limit 900/min -j ACCEPT
+
+# Allow others ICMPv6 types but only if the hop limit field is 255.
+ip6tables -A INPUT -p icmpv6 --icmpv6-type router-advertisement -m hl --hl-eq 255 -j ACCEPT
+ip6tables -A INPUT -p icmpv6 --icmpv6-type neighbor-solicitation -m hl --hl-eq 255 -j ACCEPT
+ip6tables -A INPUT -p icmpv6 --icmpv6-type neighbor-advertisement -m hl --hl-eq 255 -j ACCEPT
+ip6tables -A INPUT -p icmpv6 --icmpv6-type redirect -m hl --hl-eq 255 -j ACCEPT
+
+# When there isn't a match, the default policy (DROP) will be applied.
+# To be sure, drop all other ICMPv6 types.
+# We're dropping enough icmpv6 types to break RFC compliance.
+ip6tables -A INPUT -p icmpv6 -j LOG --log-prefix "Dropped ICMPv6"
+ip6tables -A INPUT -p icmpv6 -j DROP
+
+# Accepts all established inbound connections
+ip6tables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Allows HTTP and HTTPS connections from anywhere
+ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# Allow SSH connections 
+ip6tables -A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT
+

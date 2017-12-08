83bc4a593f5582909eac9d63d3f61824c5edf93b
Kaushik Srinivasan <kaushik.srini1994@gmail.com>
2017-12-08 17:28:49 -0500

    Copied over code from main branch. Lazy to merge. Will do later

--- pi_setup_files/dynv6.sh
+++ pi_setup_files/dynv6.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -e
 token="YOUR_DYNV6_TOKEN_HERE"
 hostname="YOUR_DOMAIN_NAME_HERE"
-device="YOUR_NETWORK_DEVICE_NAME_HERE"
+device="wlan0"
 
 if [ "$token" = "YOUR_DYNV6_TOKEN_HERE" ]; then
-  echo "Valid Authentication token missing!!"
-  exit 1
+  echo "[ERROR] Valid token is missing in the dynv6 script"
+  exit 0 
 fi
 
 
 if [ -z "$hostname" ]; then
-  echo "[ERROR] Hostname missing"
-  exit 1
+  echo "[ERROR] Domain name is missing in the dynv6 script"
+  exit 0
 fi
 
 if [ -z "$netmask" ]; then
@@ -30,16 +30,19 @@ elif [ -e /usr/bin/wget ]; then
   bin="wget -O-"
 else
   echo "neither curl nor wget found"
-  exit 1
+  exit 0
 fi
 
-if [ -z "$address" ]; then
+# Update ipv4 address with dynv6 service
+$bin "http://ipv4.dynv6.com/api/update?hostname=$hostname&ipv4=auto&token=$token"
+
+if [ ! -z "$address" ]; then
   echo "no IPv6 address found"
-  exit 1
+  exit 0
 fi
 
 # address with netmask
 current=$address/$netmask
-# send addresses to dynv6
+
+# Update ipv6 address with dynv6 service
 $bin "http://dynv6.com/api/update?hostname=$hostname&ipv6=$current&token=$token" 
-$bin "http://ipv4.dynv6.com/api/update?hostname=$hostname&ipv4=auto&token=$token"

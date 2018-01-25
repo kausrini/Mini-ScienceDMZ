2dc04289835fb01a0d744976beb8c04152fdd5a7
Advait Marathe <advait@advaits-air.attlocal.net>
2018-01-25 16:12:43 -0500

    Send status of the device to selected recipients. Final working version.

---
+++ email_config.py
@@ -0,0 +1,21 @@
+import os
+
+
+current_dir = os.getcwd()
+
+sender = ''     					# Sender's email address
+
+receiver = ''						# Receiver's email address
+
+smtp_server = 'mail-relay.iu.edu'			# Email relay to be used
+
+path_for_logs = ''					# Location for the logs to be sent e.g. /var/log/syslog
+
+
+
+# Only change the following if you want the backup logs to be stored somewhere else.
+
+path_for_backup = current_dir+"/log_copy"		# Location where these logs will be backed up just to compute the diff. Prepend user's 
+
+path_for_diff = current_dir+'/diff'			# Location where the log diff will be stored.
+
---
+++ secrets.py
@@ -0,0 +1,7 @@
+
+# This will be the username which you will use to authenticate with email relay.
+username = ""
+
+# Password for the corresponding username
+
+password = ""

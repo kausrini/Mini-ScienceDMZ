5722a24d06b61d2f531097b3f9ea2f3dfe27959b
Advait <advmarat@iu.edu>
2018-02-01 09:19:17 -0500

    Added functionality: Email multiple log files as attachments

--- email_config.py
+++ email_config.py
@@ -1,21 +1,24 @@
 import os
-
+import ntpath
 
 current_dir = os.getcwd()
 
 sender = 'minidmz@iu.edu'     					# Sender's email address
 
-receiver = ''						# Receiver's email address
-
+receiver = []						# Receiver's email address
 smtp_server = 'mail-relay.iu.edu'			# Email relay to be used
+path_for_logs = []	                                # Location for the logs to be sent e.g. /var/log/syslog
 
-path_for_logs = '/var/log/syslog'					# Location for the logs to be sent e.g. /var/log/syslog
-
-
+path_for_backup = []
+path_for_diff = []
 
 # Only change the following if you want the backup logs to be stored somewhere else.
 
-path_for_backup = current_dir+"/log_copy"		# Location where these logs will be backed up just to compute the diff. Prepend user's 
 
-path_for_diff = current_dir+'/diff'			# Location where the log diff will be stored.
+for path in path_for_logs:
+    filenm = ntpath.basename(path)
+    path_for_backup.append(current_dir+"/"+filenm+"_backup")
+    path_for_diff.append(current_dir+"/"+filenm+"_diff")
+
+
 

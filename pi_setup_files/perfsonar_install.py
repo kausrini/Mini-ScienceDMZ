83d8d09067f78d42477b2b79503f9d8d9b1665ac
Kaushik Srinivasan <kausrini@gmail.com>
2018-04-14 15:03:06 -0400

    Fixed issue with perfsonar being executed during import. Needed to add the check if the file is executed standalone or being imported.

--- pi_setup_files/perfsonar_install.py
+++ pi_setup_files/perfsonar_install.py
@@ -7,31 +7,31 @@ import sys
 
 
 def add_dependencies():
-
     print("Adding dependencies")
     # Get the source to download perfsonar if one not present already
     if not os.path.isfile('/etc/apt/sources.list.d/perfsonar-jessie-release.list'):
         try:
-            subprocess.check_output(['wget', '-P', '/etc/apt/sources.list.d/','http://downloads.perfsonar.net/debian/perfsonar-jessie-release.list'])
+            subprocess.check_output(['wget', '-P', '/etc/apt/sources.list.d/',
+                                     'http://downloads.perfsonar.net/debian/perfsonar-jessie-release.list'])
         except subprocess.CalledProcessError as err:
             print("[Error] Error adding source. Cannot continue with perfsonar installation please install it manually")
             print(err)
-            
+
     else:
         print("Source already present..skipping this step..")
 
     # Add key
     try:
         subprocess.check_call(
-        ['wget -qO - http://downloads.perfsonar.net/debian/perfsonar-debian-official.gpg.key | apt-key add -'],
-        shell=True)
+            ['wget -qO - http://downloads.perfsonar.net/debian/perfsonar-debian-official.gpg.key | apt-key add -'],
+            shell=True)
     except subprocess.CalledProcessError as err:
         print("[Error] Cannot add key. Aborting perfsonar installation. Please install it manually")
         print(err)
         sys.exit()
-        
+
     print("Updating apt")
-    
+
     # Updating apt
     try:
         subprocess.check_call(['apt-get', '-y', 'update'])
@@ -40,24 +40,25 @@ def add_dependencies():
         print(err)
         sys.exit()
 
-def install_perfsonar_testpoint():
 
+def install_perfsonar_testpoint():
     print("installing perfsonar testpoint")
 
     try:
         # Install from source added above
-        subprocess.call('sudo apt-get install --no-install-recommends perfsonar-testpoint -y',shell=True)
+        subprocess.call('sudo apt-get install --no-install-recommends perfsonar-testpoint -y', shell=True)
     except subprocess.CalledProcessError as err:
         print("[Error] Installation failed. Please install it manually")
         print(err)
         sys.exit()
 
+
 def install_additional_packages():
     packages = ['perfsonar-toolkit-ntp',
-    'perfsonar-toolkit-security',
-    'perfsonar-toolkit-servicewatcher',
-    'perfsonar-toolkit-sysctl',
-    'perfsonar-toolkit-systemenv-testpoint']
+                'perfsonar-toolkit-security',
+                'perfsonar-toolkit-servicewatcher',
+                'perfsonar-toolkit-sysctl',
+                'perfsonar-toolkit-systemenv-testpoint']
 
     print('Installing the following packages {}'.format(", ".join(packages)))
     try:
@@ -75,4 +76,6 @@ def main():
     install_perfsonar_testpoint()
     install_additional_packages()
 
-main()
+
+if __name__ == '__main__':
+    main()
